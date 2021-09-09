2975736361ace20456377218549d1e4edab977ef
Maks-Katunin <katunin87@gmail.com>
2021-09-09 13:01:48 +0600

    Попытался доделать.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,33 +4,41 @@ public class Main {
 
     public static void main(String[] args) {
 
+        createObject("Second");
+        createObject("Third");
+        createObject("Fourth");
+        createObject("Fifth");
 
-
-        Third third = new Third("tretiy",
-                3,
-                " printable ");
-        third.print();
-        System.out.println("--------------------");
-
-        Fourth fourth = new Fourth("chetvertiy",
-                4,
-                "printable",
-                "perviy");
-        fourth.print();
-        System.out.println("--------------------");
 
     }
-    public String createObject(String className){
-        switch (className){
+
+    public static void createObject(String className) {
+        switch (className) {
             case ("Second"):
-                Second second = new Second("vtoroy", 2);
+                Second second = new Second("vtoroy",
+                        2);
                 second.print();
                 System.out.println("--------------------");
                 break;
+            case ("Third"):
+                Third third = new Third("tretiy",
+                        3,
+                        " printable ");
+                third.print();
+                System.out.println("--------------------");
+                break;
+
+            case ("Fourth"):
+                Fourth fourth = new Fourth("chetvertiy",
+                        4,
+                        "printable",
+                        "perviy");
+                fourth.print();
+                System.out.println("--------------------");
+            default:
+                System.out.println("Wrond data");
         }
 
 
-
-      return" ";
     }
 }
